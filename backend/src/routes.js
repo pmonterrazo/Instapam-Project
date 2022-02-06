ab5ef5daf234d28c9bf0abb747573c85b0206408
Philip Monterrazzo <philipemonterrazo@outlook.com>
2022-02-06 20:44:33 -0300

    Iniciando o Projeto, Alterações iniciais.

---
+++ backend/src/routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+    return res.json({ message: `Hello ${req.query.name}`});
+});
+
+routes.post('/devs', (req, res) =>{
+    console.log(req.body);
+});
+
+module.exports = routes;
