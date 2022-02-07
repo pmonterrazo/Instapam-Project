66f2e71925c592b323fa673791f4a8ec359d0fe3
Philip Monterrazzo <philipemonterrazo@outlook.com>
2022-02-06 22:35:43 -0300

    Reparar erro de String no LikeController

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
+const mongoose = require('mongoose');
+
 
 const server = express();
+
+mongoose.connect('mongodb+srv://semana:semana@cluster0.z4nav.mongodb.net/omnistack?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
 
 server.use(express.json());
 server.use(routes);
